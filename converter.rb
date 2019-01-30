2e94f3efa27339443b004d92291d35ba74e9e907
Andy Islam <andy.s.islam@gmail.com>
2019-01-30 04:18:23 -0500

    added from_roman_to_numeric and enabled all tests

--- converter.rb
+++ converter.rb
@@ -4,7 +4,31 @@ require "pry"
 class Converter
 
   def self.from_roman_to_numeric roman_value
-
+    romans = {"M"=>1000, "D"=>500,"C"=>100,"L"=>50,"X"=>10,"IX"=>9, "V"=>5,"IV"=>4, "I"=>1}
+    array = roman_value.split("")
+    total = 0
+    skip = false
+    array.each_with_index do |i, index|
+      unless skip == true
+        current_num = romans["#{i}"]
+        unless (index + 1) == array.size
+          next_num = romans[array[index + 1]]
+          if next_num > current_num 
+            total = total + (next_num - current_num)
+            skip = true 
+          else
+            total = total + current_num
+            skip = false 
+          end
+        else
+          total = total + current_num
+          skip = false 
+        end
+      else 
+        skip = false
+      end
+    end
+    total
   end
 
   def self.from_numeric_to_roman numeric_value
@@ -38,33 +62,29 @@ class Converter
 end
 
 class ConverterTest < Minitest::Test
+  def test_III_is_3
+    assert_equal 3, Converter.from_roman_to_numeric("III")
+  end
 
+  def test_CM_is_900
+    assert_equal 900, Converter.from_roman_to_numeric("CM")
+  end
 
+  def test_CCXCI_is_291
+    assert_equal 291, Converter.from_roman_to_numeric("CCXCI")
+  end
 
-  # def test_III_is_3
-  #   assert_equal 3, Converter.from_roman_to_numeric("III")
-  # end
+  def test_XXIX_is_29
+    assert_equal 29, Converter.from_roman_to_numeric("XXIX")
+  end
 
-  # def test_CM_is_900
-  #   assert_equal 900, Converter.from_roman_to_numeric("CM")
-  # end
-
-  # def test_CCXCI_is_291
-  #   assert_equal 291, Converter.from_roman_to_numeric("CCXCI")
-  # end
-
-  # def test_XXIX_is_29
-  #   assert_equal 29, Converter.from_roman_to_numeric("XXIX")
-  # end
-
-  # def test_MCMXCIX_is_1999
-  #   assert_equal 1999, Converter.from_roman_to_numeric("MCMXCIX")
-  # end
+  def test_MCMXCIX_is_1999
+    assert_equal 1999, Converter.from_roman_to_numeric("MCMXCIX")
+  end
 
   def test_29_is_XXIX
     assert_equal "XXIX", Converter.from_numeric_to_roman(29)
   end
-
 
   def test_38_is_XXXVIII
     assert_equal "XXXVIII", Converter.from_numeric_to_roman(38)
